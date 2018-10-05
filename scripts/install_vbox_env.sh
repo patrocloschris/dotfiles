bcdec813bd5cc87c9b7d700075ff396da2a7c6f8
Patroklos Christou <christoupat@gmail.com>
2018-10-05 16:59:20 +0300

    Moved virtualbox packages to ansible scripts

--- scripts/install_vbox_env.sh
+++
@@ -1,4 +0,0 @@
-
-echo deb http://ftp.debian.org/debian stretch-backports main contrib > /etc/apt/sources.list.d/stretch-backports.list
-apt update
-apt install -t stretch-backports virtualbox-guest-dkms virtualbox-guest-x11 linux-headers-4.9.0-7-amd64
